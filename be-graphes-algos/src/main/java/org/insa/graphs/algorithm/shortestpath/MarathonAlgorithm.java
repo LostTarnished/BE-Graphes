8088480f09c10dcedc6404d0f207b67473c8d5a5
Laurent Morgan <laurentmorgan77@gmail.com>
2026-06-06 15:03:56 +0200

    en cours

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/MarathonAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/MarathonAlgorithm.java
@@ -5,44 +5,58 @@ import java.util.List;
 import org.insa.graphs.model.Arc;
 import org.insa.graphs.model.Node;
 
-public class MarathonAlgorithm extends ShortestPathAlgorithm{
+public class MarathonAlgorithm extends ShortestPathAlgorithm 
+{
+    private final float ACCEPTABLE = 0.1f;
 
-    public MarathonAlgorithm(ShortestPathData data) {
+    private static boolean is_acceptable = false;
+
+    public MarathonAlgorithm(ShortestPathData data) 
+    {
         super(data);
     }
 
     @Override
-    protected ShortestPathSolution doRun() {
+    protected ShortestPathSolution doRun() 
+    {
 
         // Retrieve the graph.
         ShortestPathData data = getInputData();
-        final int nbNodes = data.getGraph().size();
 
-        // Initialize array of distances.
-        Label[] distance = new Label[nbNodes];
-
-        for (int i = 0; i < nbNodes; i++) {
-            Label l = new Label(data.getGraph().getNodes().get(i),
-                    Double.POSITIVE_INFINITY);
-            distance[i] = l;
-        }
-
-        List<Arc> solution;
+        List<Arc> solution = null;
         float distance_solution = Float.POSITIVE_INFINITY;
 
+        boolean visited[] = new boolean[data.getGraph().size()];
+
         //Do the algorithm
-        DFS(data.getOrigin(), 0, solution, distance_solution, 42.125f, data.getOrigin());
+        DFS(data.getOrigin(), 0, solution, distance_solution, 42.125f, data.getOrigin(), visited);
     }
 
-    private void DFS(Node origin, float distance, List<Arc> solution, float distance_solution, float distance_marathon, Node origin_marathon) 
+    private ShortestPathSolution DFS(Node origin, float distance, List<Arc> solution, float distance_solution, float distance_marathon, Node origin_marathon, boolean[] visited) 
     {
         for (Arc arc_voisin : origin.getSuccessors()) {
-            if(arc_voisin.getDestination() == origin_marathon) {
-                if(Math.abs(distance - distance_marathon) < Math.abs(distance_solution - distance_marathon)) {
-                    // Meilleur chemin trouver
+            Node destination = arc_voisin.getDestination();
+
+            if(!visited[destination.getId()]) {
+                visited[destination.getId()] = true;
+
+                if(destination == origin_marathon) {
+                    if(Math.abs(distance - distance_marathon) < Math.abs(distance_solution - distance_marathon)) {
+                        if(Math.abs(distance - distance_marathon) <= ACCEPTABLE) {
+                            is_acceptable = true;
+                        }   
+                        // Meilleur chemin trouver
+                        return 
+                    }
+                } else {
+                    // Ce chemin peut mener à une meilleur solution
+                    if(Math.abs(distance) < Math.abs(distance_solution)) {
+
+                        solution.add(arc_voisin);
+                        DFS(destination, distance + arc_voisin.getLength(), solution, distance_solution, distance_marathon, origin_marathon, visited);
+
+                    }
                 }
-            } else {
-                // Verification condition pour continuer
             }
         }
     }
